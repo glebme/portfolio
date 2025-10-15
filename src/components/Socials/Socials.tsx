c5e545e1a49cc98e82343751368bb7da55b7a5af
Tommy Truong <glebme@yahoo.com>
2025-10-15 21:45:24 +1100

    Increase Social Link accessibility
    Increase social icon size
    Add aria-label to anchor tags

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -4,14 +4,20 @@ import {Facebook, Instagram, Linkedin} from "react-feather";
 const Socials = () => {
     return (
         <div className="socials">
-            <a className="socials__link" href="https://www.facebook.com/profile.php?id=61575123347445" target="_blank" rel="noopener noreferrer">
-                <Facebook/>
+            <a className="socials__link" href="https://www.facebook.com/profile.php?id=61575123347445" target="_blank"
+               rel="noopener noreferrer"
+               aria-label="Facebook Profile">
+                <Facebook size="32"/>
             </a>
-            <a className="socials__link" href="https://www.instagram.com/gleberss/" target="_blank" rel="noopener noreferrer">
-                <Instagram/>
+            <a className="socials__link" href="https://www.instagram.com/gleberss/" target="_blank"
+               rel="noopener noreferrer"
+               aria-label="Instagram Profile">
+                <Instagram size="32"/>
             </a>
-            <a className="socials__link" href="https://www.linkedin.com/in/tommy-huy-gia-truong/" target="_blank" rel="noopener noreferrer">
-                <Linkedin/>
+            <a className="socials__link" href="https://www.linkedin.com/in/tommy-huy-gia-truong/" target="_blank"
+               rel="noopener noreferrer"
+               aria-label="LinkedIn Profile">
+                <Linkedin size="32"/>
             </a>
         </div>);
 };
