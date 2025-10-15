593bd550b428e1da9d939a293a29177480a2aa02
Tommy Truong <glebme@yahoo.com>
2025-10-15 22:59:26 +1100

    Add a button for resume

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -16,6 +16,9 @@ const Landing = () => {
             <div className={`landing__content ${hasJustLanded ? 'landing__content--initial' : ''}`}>
                 <p className="landing__text">Get in touch with me here</p>
                 <Socials/>
+                <button className="landing__button">
+                    <a className="landing__link" href="/resources/tommy-huy-gia-truong-resume.pdf" download>View my resume</a>
+                </button>
             </div>
         </div>
     );
