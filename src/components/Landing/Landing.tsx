1f0ca2ceebb639d9cac5a33029a75dff712a8e89
Tommy Truong <glebme@yahoo.com>
2025-10-15 21:37:09 +1100

    Implement basic transitions for the landing page

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,14 +1,24 @@
 import './Landing.css'
 import Socials from "../Socials/Socials.tsx";
+import {useEffect, useState} from 'react';
 
 const Landing = () => {
+    const [hasJustLanded, setHasJustLanded] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setHasJustLanded(false), 1000);
+        return () => clearTimeout(timer);
+    });
+
     return (
         <div className="landing">
-            <h1 className="landing__heading">Tommy</h1>
-            <p className="landing__text">Get in touch with me here</p>
-            <Socials/>
+            <h1 className={`landing__heading ${hasJustLanded ? 'landing__heading--initial' : ''}`}>Tommy</h1>
+            <div className={`landing__content ${hasJustLanded ? 'landing__content--initial' : ''}`}>
+                <p className="landing__text">Get in touch with me here</p>
+                <Socials/>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Landing;
